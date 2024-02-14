1f8e44ffd5020e3573b2a46c15b82ccf456c3a16
Novip1906 <79663169+Novip1906@users.noreply.github.com>
2024-02-14 17:24:34 +0300

    1 task update

--- 1.py
+++ 1.py
@@ -2,21 +2,27 @@ import csv
 from datetime import datetime
 a = []
 d0 = '01.01.2002'
+d = '12.05.2023'
 with open('songs.csv') as file:
     #print(file.read())
     reader = csv.DictReader(file, delimiter=';')
     for s in reader.reader:
         a.append(s)
     del a[0]
+res = []
 for i in range(len(a)):
+    d1 = datetime.strptime(d, '%d.%m.%Y')
+    d00 = datetime.strptime(d0, '%d.%m.%Y')
+    d2 = datetime.strptime(a[i][3], '%d.%m.%Y')
     if a[i][0] == '0':
-        d1 = datetime.strptime(d0, '%d.%m.%Y')
-        d2 = datetime.strptime(a[i][3], '%d.%m.%Y')
         dl = (d1 - d2).days
         a[i][0] = str(abs(dl // (len(a[i][1]) + len(a[i][2]))) * 10000)
+        if d2 > d00:
+            continue
+    res.append(a[i])
+
 with open('songs_new.csv', 'w') as file:
-    fieldnames=['streams',	'artist_name', 'track_name', 'date']
     rs = 'streams;artist_name;track_name;date\n'
-    for s in a:
+    for s in res:
         rs += ';'.join(s) + '\n'
     file.write(rs)
