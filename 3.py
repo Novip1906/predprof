2ba4ecb92803200acec98415a4db2c80fcebc52c
Novip1906 <79663169+Novip1906@users.noreply.github.com>
2024-02-14 16:38:40 +0300

    3 task completed

---
+++ 3.py
@@ -0,0 +1,20 @@
+import csv
+a = []
+with open('songs.csv') as file:
+    reader = csv.DictReader(file, delimiter=';')
+    for s in reader.reader:
+        a.append(s)
+    del a[0]
+
+inp = input('Введите имя артиста: ')
+while len(inp) > 0 and inp != '0':
+    for s in a:
+        if inp.strip() in s[1].split()[0] or inp.strip() == s[1]:
+            print(f'У {s[1]} найдена песня: {s[2]}')
+            break
+    else:
+        print('К сожалению, ничего не удалось найти')
+    inp = input('Введите имя артиста: ')
+
+
+
